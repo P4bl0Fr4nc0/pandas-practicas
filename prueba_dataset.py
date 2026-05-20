74be0d3811a3f9eed4a3963ff372884e2cf9bd39
Pablo Franco <lize293@hotmail.com>
2026-05-20 07:27:55 -0600

    prueba data set

--- prueba_dataset.py
+++ prueba_dataset.py
@@ -1,3 +1,7 @@
 import pandas as pd 
 df = pd.read_csv('dataset_ventas_pandas.csv')
+print (df.dtypes)
+print (df.info())
 print(df.to_string())
+#print(df.head())   devuelve los encabezados y las primeras 5 filas
+# print(df.tail())  devuelce las ultimas 5 filas de la tabla 
